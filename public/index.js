7df674ac774f373972186f2e908fc991671326a0
root <root@Mono>
2023-08-24 00:35:59 -0500

    feat: fix mobile nav bar

--- public/index.js
+++ public/index.js
@@ -4,9 +4,4 @@ const menu = document.querySelector(".nav__menu");
 btn.addEventListener("click", () => {
   const isExpanded = menu.getAttribute("aria-expanded") === "true";
   menu.setAttribute("aria-expanded", isExpanded ? "false" : "true");
-  if (isExpanded) {
-    menu.classList.replace("nav__menu--expanded", "nav__menu")
-  } else {
-    menu.classList.replace("nav__menu", "nav__menu--expanded")
-  }
 });
